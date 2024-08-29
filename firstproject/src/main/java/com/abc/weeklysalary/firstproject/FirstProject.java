df88de3cdc068f535391c1fa720648c23614e191
shreyjay <shreya.kumbhani@yahoo.com>
2024-08-29 14:59:09 -0500

    upload project

---
+++ firstproject/src/main/java/com/abc/weeklysalary/firstproject/FirstProject.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.abc.weeklysalary.firstproject;
+
+/**
+ *
+ * @author hemap
+ */
+public class FirstProject {
+     public int add(int x, int y) {
+        return x + y;
+    }
+    
+    public int mul(int x, int y) {
+        return x * y;
+    }
+}
